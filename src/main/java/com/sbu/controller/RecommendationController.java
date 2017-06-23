4462f3d5636d62f58bebd9a6a5fbba348fc7410e
user <ad.ardeshir@gmail.com>
2017-06-23 17:21:09 +0430

    Ardeshir: RecommendationModel , RecommendationController.

--- src/main/java/com/sbu/controller/RecommendationController.java
+++ src/main/java/com/sbu/controller/RecommendationController.java
@@ -1,17 +1,36 @@
 package com.sbu.controller;
 
+import com.sbu.controller.model.RecommendationModel;
+import com.sbu.dao.model.IntroductionEntity;
 import com.sbu.service.IntroductionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by user on 6/23/2017.
  */
+@Controller
+@RequestMapping(value = "/recommendation")
 public class RecommendationController {
 
     @Autowired
     IntroductionService introductionService;
 
-    //@RequestMapping()
+    @RequestMapping(method = RequestMethod.GET)
+    public String recommendationPage(Model model){
+        return "recommendation";
+    }
+
+    @RequestMapping(value = "/main", method = RequestMethod.POST)
+    public String recommendationSubmit(Model model , @ModelAttribute("recommendationModel")RecommendationModel recommendationModel){
+
+        IntroductionEntity recommendationEntity=new IntroductionEntity();
+
+        return "";
+    }
 
 }
---
+++ src/main/java/com/sbu/controller/model/RecommendationModel.java
@@ -0,0 +1,154 @@
+package com.sbu.controller.model;
+
+
+/**
+ * Created by user on 6/23/2017.
+ */
+public class RecommendationModel {
+
+    private String succorFirstName;
+    private String succorLastName;
+    private boolean isStudent;
+    private String succorStudentNumber;
+    private String succorPhoneNumber;
+
+    private String needyFirstName;
+    private String needyLastName;
+    private String needyStudentNumber;
+    private String needyGender;
+    private String needyEntryYear;
+    private String needyFacultyName;
+    private String otherFacultyName;
+    private String isDormSettler;
+    private String isNotDormSettler;
+    private String moreInfo;
+    private String awarenessInfo;
+
+    public String getSuccorFirstName() {
+        return succorFirstName;
+    }
+
+    public String getSuccorLastName() {
+        return succorLastName;
+    }
+
+    public Boolean getStudent() {
+        return isStudent;
+    }
+
+    public String getSuccorStudentNumber() {
+        return succorStudentNumber;
+    }
+
+    public String getSuccorPhoneNumber() {
+        return succorPhoneNumber;
+    }
+
+    public String getNeedyFirstName() {
+        return needyFirstName;
+    }
+
+    public String getNeedyLastName() {
+        return needyLastName;
+    }
+
+    public String getNeedyStudentNumber() {
+        return needyStudentNumber;
+    }
+
+    public String getNeedyGender() {
+        return needyGender;
+    }
+
+    public String getNeedyEntryYear() {
+        return needyEntryYear;
+    }
+
+    public String getNeedyFacultyName() {
+        return needyFacultyName;
+    }
+
+    public String getOtherFacultyName() {
+        return otherFacultyName;
+    }
+
+    public String getIsDormSettler() {
+        return isDormSettler;
+    }
+
+    public String getIsNotDormSettler() {
+        return isNotDormSettler;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public String getAwarenessInfo() {
+        return awarenessInfo;
+    }
+
+    public void setSuccorFirstName(String succorFirstName) {
+        this.succorFirstName = succorFirstName;
+    }
+
+    public void setSuccorLastName(String succorLastName) {
+        this.succorLastName = succorLastName;
+    }
+
+    public void setStudent(Boolean student) {
+        isStudent = student;
+    }
+
+    public void setSuccorStudentNumber(String succorStudentNumber) {
+        this.succorStudentNumber = succorStudentNumber;
+    }
+
+    public void setSuccorPhoneNumber(String succorPhoneNumber) {
+        this.succorPhoneNumber = succorPhoneNumber;
+    }
+
+    public void setNeedyFirstName(String needyFirstName) {
+        this.needyFirstName = needyFirstName;
+    }
+
+    public void setNeedyLastName(String needyLastName) {
+        this.needyLastName = needyLastName;
+    }
+
+    public void setNeedyStudentNumber(String needyStudentNumber) {
+        this.needyStudentNumber = needyStudentNumber;
+    }
+
+    public void setNeedyGender(String needyGender) {
+        this.needyGender = needyGender;
+    }
+
+    public void setNeedyEntryYear(String needyEntryYear) {
+        this.needyEntryYear = needyEntryYear;
+    }
+
+    public void setNeedyFacultyName(String needyFacultyName) {
+        this.needyFacultyName = needyFacultyName;
+    }
+
+    public void setOtherFacultyName(String otherFacultyName) {
+        this.otherFacultyName = otherFacultyName;
+    }
+
+    public void setIsDormSettler(String isDormSettler) {
+        this.isDormSettler = isDormSettler;
+    }
+
+    public void setIsNotDormSettler(String isNotDormSettler) {
+        this.isNotDormSettler = isNotDormSettler;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public void setAwarenessInfo(String awarenessInfo) {
+        this.awarenessInfo = awarenessInfo;
+    }
+}
