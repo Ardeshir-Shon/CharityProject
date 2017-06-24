fc13ee8fbf7e2f7d9556203b75e38a841907118e
user <ad.ardeshir@gmail.com>
2017-06-24 13:23:43 +0430

    Ardeshir: Migrate to GenericDTO.

--- src/main/java/com/sbu/controller/PeriodicHelperController.java
+++ src/main/java/com/sbu/controller/PeriodicHelperController.java
@@ -1,6 +1,7 @@
 package com.sbu.controller;
 
 import com.sbu.controller.model.PeriodicHelpCashModel;
+import com.sbu.controller.model.PeriodicHelpDTO;
 import com.sbu.dao.model.HelperEntity;
 import com.sbu.service.HelperService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping(value = "/periodHelp")
-public class   PeriodicHelperController {
+public class PeriodicHelperController {
 
     @Autowired
     HelperService helperService;
@@ -29,11 +30,19 @@ public class   PeriodicHelperController {
     public String periodHelpSubmit(Model model, @ModelAttribute("periodicHelpCashModel")PeriodicHelpCashModel periodicHelpCashModel){
 
         HelperEntity helperEntity=new HelperEntity();
+        PeriodicHelpDTO dto=new PeriodicHelpDTO();
+        Integer predicateCount=0;
 
         if (!periodicHelpCashModel.getFirstName().isEmpty())
+        {
             helperEntity.setName(periodicHelpCashModel.getFirstName());
-        if(!periodicHelpCashModel.getLastName().isEmpty())
+            predicateCount++;
+        }
+
+        if(!periodicHelpCashModel.getLastName().isEmpty()){
             helperEntity.setFamily(periodicHelpCashModel.getLastName());
+            predicateCount++;
+        }
         if(!periodicHelpCashModel.getOccupation().isEmpty())
             helperEntity.setOccupation(periodicHelpCashModel.getOccupation());
         if(periodicHelpCashModel.isProfessor() && !periodicHelpCashModel.getFacultyName().isEmpty())
@@ -41,17 +50,36 @@ public class   PeriodicHelperController {
                 helperEntity.setProfessorCollege(periodicHelpCashModel.getOtherFacultyName());
             else
                 helperEntity.setProfessorCollege(periodicHelpCashModel.getFacultyName());
-        if(!periodicHelpCashModel.getPhoneNumber().isEmpty())
+        if(!periodicHelpCashModel.getPhoneNumber().isEmpty()){
             helperEntity.setPhoneNumber(periodicHelpCashModel.getPhoneNumber());
+            predicateCount++;
+        }
         if(!periodicHelpCashModel.getEmail().isEmpty())
             helperEntity.setEmail(periodicHelpCashModel.getEmail());
-        if(!periodicHelpCashModel.getTimePeriod().isEmpty())
+        if(!periodicHelpCashModel.getTimePeriod().isEmpty()){
             helperEntity.setHelpPeriod(periodicHelpCashModel.getTimePeriod());
-        if(!periodicHelpCashModel.getCostOfPay().isEmpty())
+            predicateCount++;
+        }
+        if(!periodicHelpCashModel.getCostOfPay().isEmpty()){
             helperEntity.setCostOfPay(periodicHelpCashModel.getCostOfPay());
+            predicateCount++;
+        }
         if(!periodicHelpCashModel.getPassword().isEmpty() && !periodicHelpCashModel.getPasswordRepeat().isEmpty())
-            if(periodicHelpCashModel.getPassword().equals(periodicHelpCashModel.getPasswordRepeat()))
+            if(periodicHelpCashModel.getPassword().equals(periodicHelpCashModel.getPasswordRepeat())){
                 helperEntity.setPassword(periodicHelpCashModel.getPassword());
+                predicateCount+=2;
+            }
+        dto.setFilled(helperEntity);
+
+        if (predicateCount!=7)// incomplete required input
+        {
+            dto.setState(0);
+        }
+        else {
+        }
+
+
+
         return "";
     }
 
