58e5c442b00b051abc05de4de41454ae34b55961
user <ad.ardeshir@gmail.com>
2017-06-25 02:50:27 +0430

    Ardeshir: Fixed some bugs.

--- src/main/java/com/sbu/controller/HomeController.java
+++ src/main/java/com/sbu/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.sbu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by user on 6/24/2017.
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/home")
 public class HomeController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public String homePage(){
         return "home";
     }
